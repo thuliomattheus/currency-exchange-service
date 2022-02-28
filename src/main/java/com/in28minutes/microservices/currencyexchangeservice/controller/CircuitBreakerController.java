a9d2c15a1c01c8e0ccd8be85248f138f33aa98c6
Thúlio Sousa <thuliomattheus@hotmail.com>
2022-02-28 18:05:37 -0300

    Adicionando circuit breaker

--- src/main/java/com/in28minutes/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
+++ src/main/java/com/in28minutes/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
 
 @RestController
@@ -15,15 +16,32 @@ import io.github.resilience4j.retry.annotation.Retry;
 public class CircuitBreakerController {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(CircuitBreakerController.class);
+  private static final String URL_TESTE = "http://localhost:8100/currency-conversion/from/TEST/to/TE/quantity/3";
+
+  private static byte count = 1;
 
   @GetMapping("")
-  @Retry(name = "sample-api", fallbackMethod = "fallbackMethod")
-  public String callUnknownApi() {
-    LOGGER.info("Tentando acessar url desconhecida...");
-    return new RestTemplate().getForEntity("http://localhost:8889/unknown-url", String.class).getBody();
+  @Retry(name = "retry-and-fallback-api", fallbackMethod = "retryFallbackMethod")
+  public String usingRetryAndFallback() {
+    LOGGER.info("Tentando acessar url do currency-conversion... [{}]", count);
+    count++;
+    ResponseEntity<String> forEntity = new RestTemplate().getForEntity(URL_TESTE, String.class);
+    count=1;
+    LOGGER.info("Consegui acessar url do currency-conversion...");
+    return forEntity.getBody();
   }
 
-  private String fallbackMethod(Throwable t){
+  @GetMapping("/circuit-breaker")
+  @CircuitBreaker(name = "circuit-breaker-api")
+  public String usingCircuitBreaker() {
+    LOGGER.info("Tentando acessar url do currency-conversion...");
+    ResponseEntity<String> forEntity = new RestTemplate().getForEntity(URL_TESTE, String.class);
+    LOGGER.info("Consegui acessar url do currency-conversion...");
+    return forEntity.getBody();
+  }
+
+  private static String retryFallbackMethod(Throwable t){
+    count = 1;
     return "Deu ruim";
   }
 }
