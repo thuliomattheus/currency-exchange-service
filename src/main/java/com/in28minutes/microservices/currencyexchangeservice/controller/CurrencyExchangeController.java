9078c8b3f1e7a4a836f4ae66dfbbf1d2a7ef3fb2
Thúlio Sousa <thuliomattheus@hotmail.com>
2022-02-28 16:11:05 -0300

    Refatorando pacote do dto

--- src/main/java/com/in28minutes/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ src/main/java/com/in28minutes/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.in28minutes.microservices.currencyexchangeservice.CurrencyExchangeDto;
+import com.in28minutes.microservices.currencyexchangeservice.dto.CurrencyExchangeDto;
 
 @RestController
 @RequestMapping("/currency-exchange")
--- src/main/java/com/in28minutes/microservices/currencyexchangeservice/dto/CurrencyExchangeDto.java
+++ src/main/java/com/in28minutes/microservices/currencyexchangeservice/dto/CurrencyExchangeDto.java
@@ -1,4 +1,4 @@
-package com.in28minutes.microservices.currencyexchangeservice;
+package com.in28minutes.microservices.currencyexchangeservice.dto;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
