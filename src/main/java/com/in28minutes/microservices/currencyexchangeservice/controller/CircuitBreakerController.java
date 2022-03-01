b5a582952be4e6bcb9694b8b97523312fefc6446
Thúlio Sousa <thuliomattheus@hotmail.com>
2022-03-01 18:45:15 -0300

    incluindo rate limiter

--- src/main/java/com/in28minutes/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
+++ src/main/java/com/in28minutes/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 
 @RestController
@@ -19,6 +20,7 @@ public class CircuitBreakerController {
   private static final String URL_TESTE = "http://localhost:8100/currency-conversion/from/TEST/to/TE/quantity/3";
 
   private static byte count = 1;
+  private static byte rateLimiterCount = 1;
 
   @GetMapping("")
   @Retry(name = "retry-and-fallback-api", fallbackMethod = "retryFallbackMethod")
@@ -40,8 +42,24 @@ public class CircuitBreakerController {
     return forEntity.getBody();
   }
 
+  @GetMapping("/rate-limiter")
+  @RateLimiter(name = "rate-limiter-api", fallbackMethod = "rateLimiterFallbackMethod")
+  public String usingRateLimiter() {
+    LOGGER.info("Tentativa {}", rateLimiterCount);
+    rateLimiterCount = (byte) (rateLimiterCount > 5 ? rateLimiterCount-5 : rateLimiterCount+1);
+    return "Conseguiu acessar a api respeitando o rate limiter";
+  }
+
   private static String retryFallbackMethod(Throwable t){
     count = 1;
     return "Deu ruim";
+  }
+
+  // Esse método sempre será usado como circuit breaker enquanto o período do rate limiter não for atingido
+  private static String rateLimiterFallbackMethod(Throwable t){
+    rateLimiterCount=1;
+    String message = "O rate limiter foi atingido e bloqueou essa requisição";
+    LOGGER.info(message);
+    return message;
   }
 }
