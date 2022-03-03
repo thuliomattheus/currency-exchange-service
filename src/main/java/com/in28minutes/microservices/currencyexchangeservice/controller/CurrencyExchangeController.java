79a2edeb8a2b684de8f93a87a00eb8a41b8acd0d
Thúlio Sousa <thuliomattheus@hotmail.com>
2022-03-03 19:13:28 -0300

    inclusão do distributed tracing - zipkin

--- src/main/java/com/in28minutes/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ src/main/java/com/in28minutes/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -1,5 +1,7 @@
 package com.in28minutes.microservices.currencyexchangeservice.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,8 +18,11 @@ public class CurrencyExchangeController {
   @Autowired
   private Environment environment;
 
+  private static final Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
+
   @GetMapping("/from/{sourceCurrency}/to/{targetCurrency}")
   public CurrencyExchangeDto re(@PathVariable String sourceCurrency, @PathVariable String targetCurrency) {
+    logger.info("from: {} -> to: {}", sourceCurrency, targetCurrency);
     return new CurrencyExchangeDto(sourceCurrency, targetCurrency, environment.getProperty("local.server.port"));
   }
 }
